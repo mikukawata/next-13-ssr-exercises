91b5ec8b55fc340443dc4652db0d39e1b6c63f1e
Miku Kawata <kawata_miku@lightcafe.co.jp>
2024-02-14 13:39:22 +0900

    formatting

--- src/app/exercises/03-interview/page.js
+++ src/app/exercises/03-interview/page.js
@@ -15,18 +15,11 @@ function InterviewExercise() {
       <Interview />
       {isDesktop && (
         <aside>
-          <img
-            src="/gwen-artist.png"
-            alt="Portrait of the artist"
-          />
+          <img src='/gwen-artist.png' alt='Portrait of the artist' />
           <h2>About the Artist</h2>
           <p>
-            Gwen Altaria is a contemporary artist known for
-            her unique blend of traditional oil painting
-            techniques and pop culture references. Born and
-            raised in London, England, Vivi had an early
-            love for both the fine arts and the fantastical
-            worlds of video games.
+            Gwen Altaria is a contemporary artist known for her unique blend of traditional oil painting techniques and pop culture references. Born and raised
+            in London, England, Vivi had an early love for both the fine arts and the fantastical worlds of video games.
           </p>
         </aside>
       )}
